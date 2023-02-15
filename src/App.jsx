a0638c56614fb4707cee82bede07359f26f5a1f9
Ashutosh Sahoo <ashutoshsahoo27@gmail.com>
2023-02-15 23:58:49 +0530

    re-order for including intermediate project

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import MainPage from '../src/components/main-page/MainPage';
 import Carousal from './components/apps/photo-carousalApp/Carousal'
 import Faq from './components/apps/faqApp/Faq'
 import ErrorPage from './components/error-page'
+import Schedule from './components/apps/Intermediate/FootballMatch/components/Pages/Schedule/Schedule'
 import {
   createHashRouter,
   // createBrowserRouter,   // remove createHashRouter in the FUTURE added this for using it in ghPages
   RouterProvider,
 } from "react-router-dom";
+import Ecommerce from './components/apps/e-commerceApp/Ecommerce';
+import Leaderboard from './components/apps/Intermediate/FootballMatch/components/Pages/Leaderboard/Leaderboard';
+import PageNotFound from './components/apps/Intermediate/FootballMatch/components/Pages/PageNotFound/PageNotFound';
 
 
 const router = createHashRouter([
@@ -23,6 +27,27 @@ const router = createHashRouter([
   {
     path: "/faq",
     element: <Faq />,
+  },
+  {
+    path: "/ecom",
+    element: <Ecommerce />,
+  },
+  {
+    path: "/footballMatch",
+    element: <Schedule />,
+  },
+  {
+    path: "/footballMatch/schedule",
+    element: <Schedule />,
+  },
+
+  {
+    path: "/footballMatch/leaderboard",
+    element: <Leaderboard />,
+  },
+  {
+    path: "/footballMatch/*",
+    element: <PageNotFound />,
   },
 ], {
   basename: process.env.PUBLIC_URL
