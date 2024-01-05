fb3d4a8b3c6eec7577273d9711f3a96aacc45712
Ashutosh Sahoo <ashutoshsahoo27@gmail.com>
2024-01-05 23:43:50 +0530

    importing react

--- src/Learning/NamasteReact/Ep2IgnitingApp/App.js
+++ src/Learning/NamasteReact/Ep2IgnitingApp/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const heading = React.createElement('h2', {id: "heading"}, "Hello from React from App file!");
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
